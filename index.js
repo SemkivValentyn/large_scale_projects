238450c102bd5bc58e3bd38e980b69054d524840
Valentyn Semkiv <74613837+SemkivValentyn@users.noreply.github.com>
2022-11-28 00:43:39 +0200

    first state

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
+
     .then(() => {
         app.listen(PORT, () => console.log(`Server Port ${PORT}`));
     }).catch((error) => console.log(`${error} did not connect`))
