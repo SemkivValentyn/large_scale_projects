cc3a2adc752e836e543c00b41019e92c46cf9eed
Valentyn Semkiv <74613837+SemkivValentyn@users.noreply.github.com>
2022-12-13 17:07:00 +0200

    React Redux File Folder Architecture and React Router

---
+++ server/data/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userIds = [
+    new mongoose.Types.ObjectId(),
+    new mongoose.Types.ObjectId(),
+    new mongoose.Types.ObjectId(),
+    new mongoose.Types.ObjectId(),
+    new mongoose.Types.ObjectId(),
+    new mongoose.Types.ObjectId(),
+    new mongoose.Types.ObjectId(),
+    new mongoose.Types.ObjectId(),
+];
+
+export const users = [
+
+];
+
+export const posts = [
+
+];
--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from './routes/posts.js'
 import {register} from "./controllers/auth.js";
 import {createPost} from "./controllers/posts.js"
 import {verifyToken} from "./middleware/auth.js";
+import User from './models/User.js'
+import Post from "./models/Post.js";
+import {users, posts} from "./data/index.js";
 
 //Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -59,4 +62,7 @@ mongoose.connect(process.env.MONGO_URL, {
 })
     .then(() => {
         app.listen(PORT, () => console.log(`Server Port ${PORT}`));
+//Add date one time
+        User.insertMany(users);
+        Post.insertMany(posts);
     }).catch((error) => console.log(`${error} did not connect`))
