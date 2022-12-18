a586f8d8fd224e700d3edfec70192b2fb8773556
Valentyn Semkiv <74613837+SemkivValentyn@users.noreply.github.com>
2022-12-18 19:01:13 +0200

    Redux and Toolkit Installation and Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Navigate, Routes, Route} from "react-router-dom";
+import HomePage from "./scenes/homePage/HomePage";
+import LoginPage from "./scenes/loginPage/LoginPage";
+import ProfilePage from "./scenes/profilePage/ProfilePage";
 
 function App() {
     return (
         <div className="App">
-
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginPage/>}/>
+                    <Route path="/home" element={<HomePage/>}/>
+                    <Route path="/profile/:userId" element={<ProfilePage/>}/>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
