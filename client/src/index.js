73a8462bc8c04310dbbf749a7a751044ae9eb918
Valentyn Semkiv <74613837+SemkivValentyn@users.noreply.github.com>
2022-12-22 15:44:13 +0200

    Color, Theme, Dark Mode, and Styling Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import authReducer from "./state"
-import {configureStore} from "@reduxjs/toolkit";
-import {Provider} from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import authReducer from "./state/state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import {
-    persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER,
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import {PersistGate} from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/integration/react";
 
-const persistConfig = {key: "root", storage, version: 1};
-const persistedReducer = persistReducer(persistConfig, authReducer)
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
-    reducer: persistedReducer, middleware: (getDefaultMiddleware) => {
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: {
-                ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            }
-        })
-    }
-})
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode>
--- client/src/scenes/navbar/Navbar.jsx
+++ client/src/scenes/navbar/Navbar.jsx
@@ -1,6 +1,27 @@
+import {useState} from "react";
+import {
+    Box, IconButton, InputBase, Typography, Select, MenuItem, FormControl, useTheme, useMediaQuery
+} from "@mui/material";
+import {
+    Search, Message, DarkMode, LightMode, Notifications, Help, Menu, Close
+} from "@mui/icons-material";
+import {useDispatch, useSelector} from "react-redux";
+import {setMode, setLogout} from "../../state/state";
+import {useNavigate} from "react-router-dom";
+import FlexBetween from "../../componets/FlexBetween";
+
 const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
+
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+    const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral
+
+    return (<div>Navbar</div>)
 }
 export default Navbar
